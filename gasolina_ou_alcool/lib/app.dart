8c5f0acf3af51bbb8d0126639f0d69679d8fe853
marcos-inja <marcosvaas@gmail.com>
2021-09-18 20:29:22 -0300

    estrutura base

--- gasolina_ou_alcool/lib/app.dart
+++ gasolina_ou_alcool/lib/app.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-// import 'dart:math';
 
 class App extends StatefulWidget {
   const App({Key? key}) : super(key: key);
@@ -9,30 +8,65 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
+  TextEditingController _textEditingController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Gasolina ou alcool'),
+        title: Text('Gasolina ou álcool'),
       ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Padding(
-            padding: EdgeInsets.all(32),
-            child: TextField(
-              
+      body: Padding(
+        padding: EdgeInsets.all(32),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset('images/logoo.png'),
+            TextField(
               keyboardType: TextInputType.text,
               decoration: InputDecoration(
-                labelText: "Digite algo",
+                labelText: "Digite o preço da gasolina",
               ),
               style: TextStyle(
                 fontSize: 20,
               ),
+              onSubmitted: (String texto) {
+                print(texto);
+              },
+              controller: _textEditingController,
             ),
-            
-          )
-        ],
+            TextField(
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                labelText: "Digite o preço do álcool",
+              ),
+              style: TextStyle(
+                fontSize: 20,
+              ),
+              onSubmitted: (String texto) {
+                print(texto);
+              },
+              controller: _textEditingController,
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 20),
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  fixedSize: Size(800, 30), //Só para ocupar tudo msm kkk
+                  textStyle:
+                      TextStyle(fontSize: 20, fontStyle: FontStyle.italic),
+                ),
+                child: Text(
+                  "Salvar",
+                  style: TextStyle(color: Colors.white),
+                ),
+                onPressed: () {
+                  print(_textEditingController.text);
+                },
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
