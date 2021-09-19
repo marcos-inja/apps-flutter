fe00483894f24ef11c73b93bd01a89b1124c1c1c
marcos-inja <marcosvaas@gmail.com>
2021-09-19 11:15:25 -0300

    feat: regra para calculo adicionada

--- gasolina_ou_alcool/lib/app.dart
+++ gasolina_ou_alcool/lib/app.dart
@@ -8,64 +8,103 @@ class App extends StatefulWidget {
 }
 
 class _AppState extends State<App> {
-  TextEditingController _textEditingController = TextEditingController();
+  TextEditingController _controllerGasolina = TextEditingController();
+  TextEditingController _controllerAlcool = TextEditingController();
+  var _resultado = '';
+
+  void _calculoMelhor() {
+    var precoGasolina = double.tryParse(_controllerGasolina.text);
+    var precoAlcool = double.tryParse(_controllerAlcool.text);
+
+    if (precoAlcool == null || precoGasolina == null) {
+      setState(() {
+        _resultado = 'Numeros invalidos!';
+      });
+    } else {
+      if (precoGasolina == precoAlcool) {
+        setState(() {
+          _resultado = 'Qualquer um dos dois';
+        });
+      } else if (precoGasolina > precoAlcool) {
+        setState(() {
+          _resultado = 'Alcool está melhor';
+        });
+      } else {
+        setState(() {
+          _resultado = 'Gasolina está melhor';
+        });
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Gasolina ou álcool'),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(32),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Image.asset('images/logoo.png'),
-            TextField(
-              keyboardType: TextInputType.text,
-              decoration: InputDecoration(
-                labelText: "Digite o preço da gasolina",
-              ),
-              style: TextStyle(
-                fontSize: 20,
-              ),
-              onSubmitted: (String texto) {
-                print(texto);
-              },
-              controller: _textEditingController,
-            ),
-            TextField(
-              keyboardType: TextInputType.text,
-              decoration: InputDecoration(
-                labelText: "Digite o preço do álcool",
-              ),
-              style: TextStyle(
-                fontSize: 20,
-              ),
-              onSubmitted: (String texto) {
-                print(texto);
-              },
-              controller: _textEditingController,
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 20),
-              child: TextButton(
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.blue,
-                  fixedSize: Size(800, 30), //Só para ocupar tudo msm kkk
-                  textStyle:
-                      TextStyle(fontSize: 20, fontStyle: FontStyle.italic),
-                ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Image.asset('images/logoo.png'),
+              Padding(
+                padding: EdgeInsets.only(top: 20, bottom: 10),
                 child: Text(
-                  "Salvar",
-                  style: TextStyle(color: Colors.white),
+                  "Saiba qual a melhor opção para abastecer.",
+                  style: TextStyle(fontSize: 30, color: Colors.blue[900]),
                 ),
-                onPressed: () {
-                  print(_textEditingController.text);
-                },
               ),
-            ),
-          ],
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Digite o preço da gasolina",
+                ),
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+                controller: _controllerGasolina,
+              ),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Digite o preço do álcool",
+                ),
+                style: TextStyle(
+                  fontSize: 20,
+                ),
+                controller: _controllerAlcool,
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: Colors.blue,
+                    textStyle: TextStyle(fontSize: 20),
+                  ),
+                  child: Text(
+                    "Calcular",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () {
+                    _calculoMelhor();
+                  },
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Text(
+                  _resultado,
+                  style: TextStyle(
+                      color: Colors.blue[900],
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
